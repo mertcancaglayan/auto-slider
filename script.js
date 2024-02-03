2da3641caa414b4f907586580582222ba7ee8540
M.can <caglayan.mertcan37@gmail.com>
2024-02-03 21:30:35 +0300

    style, script updated

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const radioButtons = document.querySelectorAll('.slides input[type="radio"]');
 const imgContainers = document.querySelectorAll(".img-container");
+const bodyElement = document.querySelector("body");
+
+
+let currentcolor = getComputedStyle(bodyElement).backgroundColor;
+
+function animateBackgroundColor(targetColor, duration) {
+	bodyElement.animate(
+		{ backgroundColor: [currentcolor, targetColor] },
+		{
+			duration: duration,
+			easing: "ease",
+			fill: "both",
+		},
+	);
+
+	currentcolor = targetColor
+	
+}
 
 let lastBtn = 0;
 
@@ -9,15 +27,39 @@ function slide(index) {
 	let durationTime;
 
 	if (index - lastBtn >= 2 && index - lastBtn < 4) {
-		durationTime = 1000;
+		durationTime = 1750;
 	} else if (index - lastBtn >= 4) {
-		durationTime = 1500;
+		durationTime = 2250;
 	} else if (index - lastBtn <= -2 && index - lastBtn > -4) {
-		durationTime = 1000;
+		durationTime = 1750;
 	} else if (index - lastBtn <= -4) {
-		durationTime = 1500;
+		durationTime = 2250;
 	} else {
-		durationTime = 500;
+		durationTime = 1250;
+	}
+
+	switch (newPosition) {
+		case 0:
+			animateBackgroundColor("rgba(7, 12, 14, 0.808)", durationTime);
+			break;
+		case -100:
+			animateBackgroundColor("rgba(23, 66, 5, 0.808)", durationTime);
+			break;
+		case -200:
+			animateBackgroundColor("rgba(112, 21, 108, 0.808)", durationTime);
+			break;
+		case -300:
+			animateBackgroundColor("rgba(180, 125, 22, 0.808)", durationTime);
+			break;
+		case -400:
+			animateBackgroundColor("rgba(98, 112, 117, 0.808)", durationTime);
+			break;
+		case -500:
+			animateBackgroundColor("rgb(8, 43, 15)", durationTime);
+			break;
+		default:
+			animateBackgroundColor("rgba(7, 12, 14, 0.808)", durationTime);
+			break;
 	}
 
 	imgContainers.forEach((container) => {
@@ -38,9 +80,7 @@ let counter = 1;
 setInterval(() => {
 	const radioButton = document.getElementById(`btn${counter}`);
 	radioButton.checked = true;
-
 	radioButton.dispatchEvent(new Event("change"));
-
 	counter++;
 	if (counter > 7) {
 		counter = 1;
